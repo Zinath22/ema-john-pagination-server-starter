81ed1dc1a95c20ffc96fb6d1201884f74c441664
Zinath22 <Zinath22@users.noreply.github.com>
2023-11-02 10:51:57 +0600

    pagination correction added

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(express.json());
 // run().catch(console.dir);
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgjjcg0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -56,22 +56,65 @@ async function run() {
 
     const productCollection = client.db('emaJohnDB').collection('products');
 
-    app.get('/products', async(req, res) => {
+    
+    app.get('/products', async (req, res) => {
       const page = parseInt(req.query.page);
       const size = parseInt(req.query.size);
 
-      console.log('pagination', page, size);
-        const result = await productCollection.find()
-        .skip(page * size)
-        .limit(size)
-        .toArray();
-        res.send(result);
+      console.log('pagination query', page, size);
+      const result = await productCollection.find()
+      .skip(page * size)
+      .limit(size)
+      .toArray();
+      res.send(result);
     })
 
-    app.get('/productsCount', async(req, res) =>{
+    app.post('/productByIds', async(req, res) =>{
+      const ids = req.body;
+      const idsWithObjectId = ids.map(id => new ObjectId(id))
+      const query = {
+        _id: {
+          $in: idsWithObjectId
+        }
+      }
+      const result = await productCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.get('/productsCount', async (req, res) => {
       const count = await productCollection.estimatedDocumentCount();
       res.send({ count });
     })
+
+    // app.get('/products', async(req, res) => {
+    //   const page = parseInt(req.query.page);
+    //   const size = parseInt(req.query.size);
+
+    //   console.log('pagination', page, size);
+    //     const result = await productCollection.find()
+    //     .skip(page * size)
+    //     .limit(size)
+    //     .toArray();
+    //     res.send(result);
+    // })
+
+    // app.post('/productByIds', async(req, res) =>{
+    //   const ids = req.body;
+     
+    //   const idsWithObjectId = ids.map(id => new ObjectId(id))
+    //   const query = {
+    //     _id: {
+    //       $in: idsWithObjectId
+    //     }
+    //   }
+    //  const result = await productCollection.find(query).toArray()
+    //   res.send(result)
+    // })
+
+    // app.get('/productsCount', async(req, res) =>{
+    //   const count = await productCollection.estimatedDocumentCount();
+    //   res.send({ count });
+    // })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
